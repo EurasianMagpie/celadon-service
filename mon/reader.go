8568e09e6848326316524f73895266b35597aed4
EurasianMaggpie <xiejinghui@gmail.com>
2019-01-24 23:59:48 -0800

    to read anyway...

--- mon/reader.go
+++ mon/reader.go
@@ -55,6 +55,98 @@ func LoadHtml() {
 	fmt.Println(body)
 }
 
-func Read(h string) {
+///
+
+func getFirstElementByName(n *html.Node, name string) (*html.Node, error) {
+    if n.Type == html.ElementNode && n.Data == name {
+        return n, nil
+    }
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        r, err := getFirstElementByName(c, name)
+        if err==nil && r != nil {
+            return r, err
+        }
+    }
+    return nil, errors.New("Not found")
+}
+
+func getLastElementByName(doc *html.Node, name string) (*html.Node, error) {
+    var b *html.Node
+    var f func(*html.Node)
+    f = func(n *html.Node) {
+        if n.Type == html.ElementNode && n.Data == name {
+            b = n
+        }
+        for c := n.FirstChild; c != nil; c = c.NextSibling {
+            f(c)
+        }
+    }
+    f(doc)
+    if b != nil {
+        return b, nil
+    }
+    return nil, errors.New("Missing " + name + " in the node tree")
+}
+
+func getPriceTable(n *html.Node) (*html.Node, error) {
+    if n.Type == html.ElementNode && n.Data == "table" {
+        for _, a := range n.Attr {
+            if strings.Compare(a.Key, "data-search-table") == 0 {
+                return n, nil
+            }
+        }
+    }
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        ptb, err := getPriceTable(c)
+        if err==nil && ptb != nil {
+            return ptb, err
+        }
+    }
+    return nil, errors.New("Not found")
+}
+
+func getRegionHead(nodePrice *html.Node) (*html.Node, error) {
+    return getFirstElementByName(nodePrice, "tr")
+}
+
+func readRegionInfo(nodeRegion *html.Node) {
+    for c := nodeRegion.FirstChild; c != nil; c = c.NextSibling {
+        if c.Data == "th" {
+            //var title string
+            //var namespace string
+            var kv string
+            for _, a := range c.Attr {
+                if a.Key == "title" {
+                    //title = a.Val
+                    //namespace = a.Namespace
+                    kv = kv + "k:" + a.Key + " v:" + a.Val + "\n"
+                    break
+                }
+            }
+            //fmt.Println("title: " + title + " namespace: " + namespace)
+            fmt.Println(renderNode(c))
+            fmt.Println(kv)
+
+        }
+    }
+}
+
+func Read(htm string) {
+    doc, _ := html.Parse(strings.NewReader(htm))
+    nodePrice, err := getPriceTable(doc)
+    if err != nil {
+        return
+    }
+    fmt.Println(nodePrice)
+    //price := renderNode(nodePrice)
+    //fmt.Println(price)
     
+    nodeRegion, err := getRegionHead(nodePrice)
+    if err != nil {
+        return
+    }
+    //fmt.Println(nodeRegion)
+    region := renderNode(nodeRegion)
+    fmt.Println(region)
+    readRegionInfo(nodeRegion)
 }
